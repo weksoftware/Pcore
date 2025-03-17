dc94509b841917d82408e2855be8385f86e50d46
mrwek <michele@weks.dev>
2025-03-18 03:36:17 +0700

    Обновлён номер версии

--- level_three/data.lua
+++ level_three/data.lua
@@ -7,6 +7,6 @@ data.block = 1
 data.blocks_for_building = {"air", "water", "sand", "steel", "clay", "debug", "glass", "lamp", "cypress_wood", "cypress_leaves", "cypress_cone", "dirt", "grass", "stone", "cable"}
 data.display_debug = true
 data.coord_for_rect = nil
-data.version = "0.0.0.1"
+data.version = "0.0.0.2"
 
 return data
