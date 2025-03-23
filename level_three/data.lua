761cac8cf3e723ae5f565b2b214136a81e6571ca
mrwek <michele@weks.dev>
2025-03-24 01:59:20 +0700

    Добавлен блок кипарисовых досок

--- level_three/data.lua
+++ level_three/data.lua
@@ -4,7 +4,7 @@ data.message = ""
 data.settings = {shader = nil, name = 'mrwek', multiplayer = false}
 data.planet = "pcore"
 data.block = 1
-data.blocks_for_building = {"air", "water", "sand", "steel", "clay", "debug", "glass", "lamp", "cypress_wood", "cypress_leaves", "cypress_cone", "dirt", "grass", "stone", "cable", "oxygen", "solar_panel", "electric_furnance"}
+data.blocks_for_building = {"air", "water", "sand", "steel", "clay", "debug", "glass", "lamp", "cypress_wood", "cypress_leaves", "cypress_cone", "dirt", "grass", "stone", "cable", "oxygen", "solar_panel", "electric_furnance", "cypress_planks"}
 data.display_debug = true
 data.coord_for_rect = nil
 data.version = "0.0.0.4"
