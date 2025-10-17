dda7b632afede9d6656020f73025958b8d0d7d87
mrwek <michele@weks.dev>
2025-10-17 23:09:16 +0700

    Добавлена команда списка игроков

--- engine/modules/graphics/chat/commands.lua
+++ engine/modules/graphics/chat/commands.lua
@@ -1,6 +1,7 @@
 local funcs = require("engine/core/funcs")
 local player = require("engine/modules/player/player")
 local data = require("engine/core/data")
+local players = require("engine/modules/multiplayer/players")
 
 local commands = {}
 
@@ -15,6 +16,15 @@ end
 
 function commands.rndshader()
     data.settings.shaders = love.math.random(#data.settings_values.shaders)
+end
+
+function commands.players()
+    funcs.create_message(player, nil, "Список текущих игроков:", os.clock(), 152, 255, 92)
+
+    funcs.create_message(player, nil, data.settings.nickname, os.clock(), 152, 255, 92)
+    for _, player_data in pairs(players) do
+        funcs.create_message(player, nil, player_data.nickname, os.clock(), 152, 255, 92)
+    end
 end
 
 
